0661691ea545db19cba88c7516765dcdac97fdc8
samerabdelkafi <samer.abdelkafi@gmail.com>
2025-04-08 12:12:35 +0200

    fix test

--- messages-router-front/src/app/messages/services/message.service.spec.ts
+++ messages-router-front/src/app/messages/services/message.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MessageService } from './message.service';
+import { MessageAuditService } from './message.service';
 
 describe('MessagesService', () => {
-  let service: MessageService;
+  let service: MessageAuditService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(MessageService);
+    service = TestBed.inject(MessageAuditService);
   });
 
   it('should be created', () => {
